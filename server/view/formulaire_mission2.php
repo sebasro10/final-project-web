a8597b22e87d44614b16c450ed505e8024f0457d
Marc2Serre <80419808+Marc2Serre@users.noreply.github.com>
2022-03-16 17:16:20 +0100

    Add files via upload

---
+++ server/view/formulaire_mission2.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+ <?php 
+include_once("db-functions.php");
+include_once("config.php");
+
+ ?>
+    <h1>Création d'une mission</h1>
+<form method = "post" name="FrameCitation" action = <?php echo $_SERVER['PHP_SELF'];?>>
+<div>
+  <label for="nom">Nom :</label>
+  <input type="text" autocomplete = on required = "true" id="nom" name="nom_mission">
+</div>
+<div>
+  <label for="lieu">Lieu :</label>
+    <input type="text" autocomplete = on required = "true" id="lieu" name="lieu_mission">
+</div>
+  <div>
+    <label for = "date de debut">Date de début :</label>
+ <input name = "date_debut" type = "date">
+     <label for = "date de fin">Date de fin :</label>
+ <input name = "date_fin" type = "date">
+  </div>
+<div>
+  <label for="description">Description :</label>
+  <textarea id="description" name="description_mission">
+</textarea>
+</div>
+<div>
+  <input name="Enregistrer" value="Enregistrer la mission" type="submit">
+</div>
+</form>
+<?php if (isset($_POST['nom_mission']) AND $_POST['lieu_mission'] != ""
+     AND isset($_POST['date_debut']) AND $_POST['date_fin'] != ""
+     AND isset($_POST['description_mission'])){
+
+	$nom = $_POST['nom_mission'];
+	$lieu = $_POST['lieu_mission'];
+	$date_debut = $_POST['date_debut'];
+	$date_fin = $_POST['date_fin'];
+	$description = $_POST['description_mission'];
+	$statut = "en cours";
+	
+	$user = 1; //$_SESSION['user_id'];
+
+	insert_mission($nom, $statut , $lieu, $date_debut, $date_fin, $description, $user);
+	
+	echo "mission ajoutée";
+ }
+?>
+
+<a href = 'tableau_missions2.php'>retour visualisation missions</a>
+</body>
+</html>
---
+++ server/view/tableau_missions2.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport"
+content="width=device-width">
+<title>Visualisation_missions</title>
+</head>
+<body>
+<h1>Missions</h1>
+
+<table border="1" bgcolor="#ccccff" frame="above">
+ <tbody>
+    <tr>
+
+<td>Nom</td>
+<td>Statut</td>
+<td>Actions</td>
+</tr>
+<?php
+
+include("db-functions.php");
+include_once("config.php");
+
+$pdo = connect();
+$sql="SELECT * FROM missions";
+        //Interact with database
+$pdoStatement = $pdo->query($sql);
+while ($row = $pdoStatement->fetch()){ ?> <tr><td><?php
+	echo $row['name'] ?></td>
+	<td> <?php echo $row['status'] ?></td>
+	<td>
+		<button id = "visualiser" name="visualiser" onclick = "location.href = 'à changer.php';">visualiser</button>
+		<button id = "cloturer" name="cloturer" onclick = "<?php cloturer($row['id_mission']) ?>">cloturer</button>
+		<button id = "modifier" name="modifier" 
+			onclick = "location.href = 'modification_mission.php?nom=<?php echo $row['name'] 
+			?>&lieu=<?php echo $row['place'] 
+			?>&debut=<?php echo $row['start_date'] 
+			?>&fin=<?php echo $row['end_date'] 
+			?>&description=<?php echo $row['description']
+			?>&id_mission=<?php echo $row['id_mission'] ?>';">modifier</button>
+		<button id = "supprimer" name="supprimer" onclick = "<?php supprimer($row['id_mission']) ?>">supprimer</button>
+	</td>
+	</tr><?php }
+disconnect($pdo);
+?>
+
+</tbody>
+</table>
+
+<button onclick = "location.href = 'formulaire_mission2.php';">Nouvelle Mission</button>
+
+</body>
+</html>
