fc10f6c3804cd8e3986f1134043007f0b9e1e3e6
Sebastian Romero <sebastian.romero@imt-atlantique.net>
2022-03-16 15:36:29 +0100

    homeController

---
+++ server/controller/HomeController.class.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once 'controller/Controller.class.php';
+
+class HomeController extends Controller {
+	
+	function index() {
+		include_once 'view/login.php';
+	}
+
+}
+
+?>
--- server/index.php
+++ server/index.php
@@ -2,46 +2,55 @@
 
 set_include_path(__DIR__);
 
-include_once 'model/Citation.class.php';
-include_once 'model/Author.class.php';
+include_once 'model/Cost.class.php';
+include_once 'model/Credit.class.php';
+include_once 'model/Mission.class.php';
+include_once 'model/Service.class.php';
 include_once 'model/User.class.php';
 include_once 'controller/HomeController.class.php';
-include_once 'controller/CitationsController.class.php';
-include_once 'controller/UsersController.class.php';
 
 $homeController = new HomeController();
-$citationsController = new CitationsController();
-$usersController = new UsersController();
 
 session_start();
 if(!isset($_SESSION["login"])){
-    if (isset($_GET['action']) && $_GET['action'] == 'inscription') {
-        $usersController->addUser();
-    } else {
-        $usersController->index();
-    }
+    // if (isset($_GET['action']) && $_GET['action'] == 'inscription') {
+    //     $usersController->addUser();
+    // } else {
+    //     $usersController->index();
+    // }
 } else if (isset($_GET['action'])) {
     $action = $_GET['action'];
     switch ($action) {
-        case 'viewCitations':
-            $citationsController->index();
-            break;
-        case 'viewCitation':
-            $citationsController->viewCitation();
-            break;
-        case 'addCitation':
-            $citationsController->addCitation();
-            break;
-        case 'home':
+        case 'login':
             $homeController->index();
             break;
+        case 'signup':
+            break;
+        case 'countability':
+            break;
+        case 'viewMissions':
+            break;
+        case 'viewMission':
+            break;
+        case 'endMission':
+            break;
+        case 'updateMission':
+            break;
+        case 'deleteMission':
+            break;
+        case 'addMission':
+            break;
+        case 'addOperation':
+            break;
+        case 'viewCosts':
+            break;
         case 'logout':
-            $_SESSION = array();
-            session_destroy();
-            $usersController->index();
+            // $_SESSION = array();
+            // session_destroy();
+            // $usersController->index();
             break;
         default:
-            $usersController->index();
+            $homeController->index();
             break;
     }
 } else {
