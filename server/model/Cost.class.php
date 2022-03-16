45afd18db0045b988413e25dbfb2c7692ed93fb4
Sebastian Romero <sebastian.romero@imt-atlantique.net>
2022-03-16 12:18:45 +0100

    class cost

--- server/model/Cost.class.php
+++ server/model/Cost.class.php
@@ -9,7 +9,7 @@ class Cost extends DataModel {
     }
 
     function readRecords() {
-        $sql = 'select * from citations';
+        $sql = 'select * from costs';
         $this->statement = $this->conn->query($sql);
         $this->statement->setFetchMode(PDO::FETCH_ASSOC);
         $records = $this->statement->fetchAll();
@@ -17,32 +17,35 @@ class Cost extends DataModel {
     }
 
     function deleteRecord($id) {
-        $sql = "delete from citations where id='$id'";
+        $sql = "delete from costs where id_cost='$id'";
         $this->statement = $this->conn->query($sql);
         $success = $this->statement->execute();
         return $success;
     }
 
     function insertRecord($values) {
-        $sql = 'insert into citations (id_user, id_author, citation, citationDate) values(?,?,?,?)';
+        $sql = 'insert into costs (date, description, quantity, currency, id_mission, evidence, type_of_expense) values(?,?,?,?,?,?,?)';
         $this->statement = $this->conn->prepare($sql);
         $success = $this->statement->execute($values);
         return $success;
     }
 
     function updateRecord($id, $values) {
-        $sql = "update citations set id_user=?,
-            id_author=?,
-            citation=?,
-            citationDate=?
-            where id=$id";
+        $sql = "update costs set date=?,
+            description=?,
+            quantity=?,
+            currency=?,
+            id_mission=?,
+            evidence=?,
+            type_of_expense=?
+            where id_cost=$id";
         $this->statement = $this->conn->prepare($sql);
         $success = $this->statement->execute($values);
         return $success;
     }
 
     // function getRecordById($id) {
-    //     $sql = "select * from citations where id='$id'";
+    //     $sql = "select * from costs where id='$id'";
     //     $this->statement = $this->conn->prepare($sql);
     //     $success = $this->statement->execute();
     //     if ($success) {
